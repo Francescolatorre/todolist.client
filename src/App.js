2954bc37f6974b0798f2be562211c50287daaa8d
Francesco G. La Torre <Francesco.Latorre@maibornwolff.de>
2019-01-15 22:10:01 +0100

    adding missing files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from "./todolist/TodoList";
+import NewTodo from "./todolist/newtodo/NewTodo";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    serverUrl = "https://jsonplaceholder.typicode.com/todos/";
+
+  componentDidMount = () => {
+      console.log("enter App:componentDidMount");
   }
+
+  render = () => {
+      return (
+          <div>
+              <h1 align={"center"}>Software Testing <br/> TodoList</h1>
+              <hr/>
+              <TodoList/>
+              <hr/>
+              <div align={"Center"}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Hochschule_RheinMain_logo.svg/2000px-Hochschule_RheinMain_logo.svg.png" width={200}/></div>
+          </div>
+      )
+  }
+
 }
 
 export default App;
